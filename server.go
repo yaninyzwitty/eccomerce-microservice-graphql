2a47610d049765902f059b9cfb8a7bb315a0d6f7
yaninyzwitty <ianmwa143@gmail.com>
2024-11-24 23:14:14 +0300

    add db connection

--- server.go
+++ server.go
@@ -14,11 +14,22 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"github.com/yaninyzwitty/new-galgrn-go/database"
 	"github.com/yaninyzwitty/new-galgrn-go/graph"
 	"github.com/yaninyzwitty/new-galgrn-go/pkg"
 )
 
+var (
+	username string
+	password string
+	hostname string
+)
+
 func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
 	var cfg pkg.Config
 	file, err := os.Open("config.yaml")
 	if err != nil {
@@ -30,6 +41,41 @@ func main() {
 		slog.Error("failed to load config")
 		os.Exit(1)
 	}
+
+	err = godotenv.Load()
+	if err != nil {
+		slog.Error("failed to load .env file")
+	}
+
+	username = os.Getenv("USERNAME")
+	password = os.Getenv("PASSWORD")
+	hostname = os.Getenv("HOSTNAME")
+	if username == "" || password == "" || hostname == "" {
+		slog.Error("failed to load mongo db config")
+		os.Exit(1)
+	}
+
+	mongoDBConfig := &database.MongoDBConfig{
+		Username: username,
+		Password: password,
+		Hostname: hostname,
+	}
+
+	mongoDBClient, err := database.NewMongoDbConnection(ctx, mongoDBConfig)
+	if err != nil {
+		slog.Error("failed to create mongo db connection: ", "error", err)
+		os.Exit(1)
+	}
+	defer mongoDBClient.Disconnect(ctx)
+
+	err = database.PingDatabase(ctx, mongoDBClient)
+	if err != nil {
+		slog.Error("failed to ping database: ", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("connected to database succesfully 🚀")
+
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
@@ -55,7 +101,7 @@ func main() {
 	<-stopCH
 
 	slog.Info("shuttting down the server...")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	if err := server.Shutdown(ctx); err != nil {
 		slog.Error("failed to shut down the server", "error", err)
